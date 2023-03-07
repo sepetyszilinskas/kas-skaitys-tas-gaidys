c52f181f7be4110a650c7bed920cc161de42ece6
sepetyszilinskas <127238836+sepetyszilinskas@users.noreply.github.com>
2023-03-07 19:52:03 +0200

    Add files via upload

---
+++ Blackjackquest.py
@@ -0,0 +1,69 @@
+import random
+import game_engine as game
+
+playa = game.Game_engine()
+dealer = game.Game_engine()
+
+kalade = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K",
+              "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K",
+              "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K",
+              "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+
+
+
+def check_if_bust():
+    if playa.total > 21:
+        playa.bust = True
+    if dealer.total > 21:
+        dealer.bust = True
+
+
+def check_score():
+    if playa.total > dealer.total and not playa.bust:
+        print("player wins")
+    elif playa.total < dealer.total and not dealer.bust:
+        print("house wins")
+    else:
+        print("DRAW")
+
+
+def draw(player):
+    korta = random.choice(kalade)
+    player.hand.append(korta)
+    kalade.remove(korta)
+
+    if korta in ["J", "Q", "K"]:
+        player.total += 10
+    elif korta == "A":
+        if player.total < 11:
+            player.total += 11
+        else:
+            player.total += 1
+    else:
+        player.total += korta
+
+def run_game():
+    turn_counter = 1
+    while turn_counter < 10:
+        if turn_counter % 2 == 0 and playa.is_hitting and not playa.bust:
+            action = int(input("Input 2 for stand, input 1 for hit : "))
+            if action == 1:
+                draw(playa)
+                print("u drew", playa.hand[-1])
+            else:
+                playa.is_hitting = False
+            print(playa.hand)
+
+        else:
+            if dealer.total < 17:
+                draw(dealer)
+                print("dealer", dealer.hand)
+            else:
+                dealer.is_hitting = False
+                if playa.bust or playa.is_hitting == False:
+                    check_score()
+                    break
+        check_if_bust()
+        turn_counter += 1
+
+
